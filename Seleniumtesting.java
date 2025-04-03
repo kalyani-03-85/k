5a6be598b56fff93bd3be888d60687bf1945b910
kalyani-03-85 <kalyani.kajjam@gmail.com>
2025-04-02 21:23:11 -0400

    Add files via upload

---
+++ Seleniumtesting.java
@@ -0,0 +1,19 @@
+package selenium;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleniumtesting {
+
+	public static void main(String[] args) { 
+		
+		//ChromeDriver driver= new ChromeDriver();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://amazon.com");
+		
+			}
+
+	}
+
+
